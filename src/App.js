7ba16a1b938c357ed3191b1324345a2d2b67e0aa
hasib2019 <hasib.9437.hu@gmail.com>
2023-04-02 14:54:50 +0600

    update samity final

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 import React from 'react';
-import Home from './pages/Home';
-import AddTask from './pages/AddTask';
-import EditTask from './pages/EditTask';
-import './assets/styles/main.css';
+import StudentLogin from './pages/StudentLogin';
+import StudentReistration from "./pages/StudentReistration"
+import './assets/styles/output.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Quiz from './pages/Quiz';
+import CoursePlayer from './pages/CoursePlayer';
+import Leaderboard from './pages/Leaderboard';
+import AdminLogin from './pages/admin/AdminLogin';
+import Assignment from './pages/admin/Assignment';
+import AssignmentMark from './pages/admin/AssignmentMark';
+import Dashboard from './pages/admin/Dashboard';
+import Quizzes from './pages/admin/Quizzes';
+import Videos from './pages/admin/Videos';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="/add-task" element={<AddTask />} />
-        <Route path="/edit-task/:taskId" element={<EditTask />} />
+        {/* student panel  */}
+        <Route path='/' element={<StudentLogin />} />
+        <Route path="/student-registration" element={<StudentReistration />} />
+        <Route path="/leader-board" element={<Leaderboard />} />
+        <Route path="/course-player" element={<CoursePlayer />} />
+        <Route path="/quiz" element={<Quiz />} />
+        {/* Admin panel  */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/assignment" element={<Assignment />} />
+        <Route path="/admin/assignment-mark" element={<AssignmentMark />} />
+        <Route path="/admin/quizzes" element={<Quizzes />} />
+        <Route path="/admin/videos" element={<Videos />} />
       </Routes>
     </Router>
   );
