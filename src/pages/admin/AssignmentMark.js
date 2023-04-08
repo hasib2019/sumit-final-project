df421cd41c73fbdf1684aa2772a98b084b70d1b6
hasib2019 <hasib.9437.hu@gmail.com>
2023-04-08 14:23:35 +0600

    udpate

--- src/pages/admin/AssignmentMark.js
+++ src/pages/admin/AssignmentMark.js
@@ -1,9 +1,9 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import Marks from '../../components/admin/assignmentMark/Marks'
 const AssignmentMark = () => {
-    return (
-      <Marks/>  
-    )
+  return (
+    <Marks />
+  )
 }
 
 export default AssignmentMark
